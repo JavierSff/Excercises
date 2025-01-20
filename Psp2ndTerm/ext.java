f44fb382bbb866a46b7905355239fc5bc07e9100
Javier <javiersf92@gmail.com>
2025-01-20 09:06:51 +0100

    modified ext and imp files with the teacher's specifications

---
+++ Psp2ndTerm/ext.java
@@ -0,0 +1,44 @@
+    package Psp2ndTerm;
+	
+	public class ext {
+	
+	    static class Writer extends Thread {
+	        private String authorName;
+	        private int estimatedTime; 
+	
+	     
+	        public Writer(String authorName, int estimatedTime) {
+	            this.authorName = authorName;
+	            this.estimatedTime = estimatedTime;
+	        }
+	
+	        @Override
+	        public void run() {
+	            System.out.println(authorName + " started writing.");
+	            try {
+	       
+	                Thread.sleep(estimatedTime * 1000L); 
+	            } catch (InterruptedException e) {
+	                System.out.println(authorName + " was interrupted while writing.");
+	            }
+	            System.out.println(authorName + " finished writing.");
+	        }
+	    }
+	
+	    public static class MultiThreadWriter {
+	        public static void main(String[] args) {
+	         
+	            Writer writer1 = new Writer("Louis the Journalist", 3);
+	            Writer writer2 = new Writer("Thomas the Writer", 5);
+	            Writer writer3 = new Writer("Nadia the Intern", 10);
+	
+	         
+	            writer1.start();
+	            writer2.start();
+	            writer3.start();
+	
+	       
+	            System.out.println("All writers are writing...");
+	        }
+	    }
+	}
---
+++ Psp2ndTerm/imp.java
@@ -0,0 +1,45 @@
+	package Psp2ndTerm;
+	
+	public class imp {
+	
+		static class Runners implements Runnable {
+			private String runnerName;
+			private int recordTime;
+		
+		public Runners (String runnerName, int recordTime) {
+			this.runnerName = runnerName;
+			this.recordTime = recordTime;
+		}
+		@Override
+		public void run() {
+			
+			System.out.println(runnerName + "started running.");
+			try {
+				Thread.sleep(recordTime*1000L);
+			}catch (InterruptedException e){
+				
+				System.out.println(runnerName + "was interrupted while runnig.");
+				
+			}
+			System.out.println(runnerName + "arrived to the finish line.");
+		}
+	}
+	
+	public static void main(String[] args) {
+		        
+		    Runners runner1 = new Runners("Luis M.", 3);
+		    Runners runner2 = new Runners("Mario S.", 5); 
+		    Runners runner3 = new Runners("Ester L.", 6); 
+		    
+		    Thread thread1 = new Thread(runner1);
+		    Thread thread2 = new Thread(runner2);
+		    Thread thread3 = new Thread(runner3);
+		    
+		    thread1.start();
+		    thread2.start();
+		    thread3.start();
+			
+		    System.out.println("All Runners are running...");
+		}
+	}
+	
